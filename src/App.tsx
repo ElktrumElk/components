892ec2ed3f0bef45f6fb8bb974793516cd69d04f
ElktrumElk <festinacole373@gmail.com>
2026-07-23 18:41:47 +0000

    new component update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
+import { Layout } from "./icons";
 import Center from "./lib/components/center/Center";
 import Container from "./lib/components/container/Container";
 import Header from "./lib/components/header/Header";
+import Icon from "./lib/components/icon/Icon";
 import Page from "./lib/components/page/Page";
+
 import Text from "./lib/components/text/Text";
+//import { createStore, useStore} from "./hooks";
+//import { Layout } from "./icons";
+
+//const store = createStore<{ count: number }>({ count: 0 });
 
 export default function App() {
   return (
-    <>
-      <Page
-        header={() => (
-          <Header
-            title={() => (
-              <Text color="white" text="Component" size="1rem" type="h1" />
-            )}
-            subTitle={() => (
-              <Text color="grey" text="Component"  size=".9rem" type="h2" />
-            )}
-            style={{ maxWidth: "400px" }}
-          />
-        )}
-        body={() => <Center child={() => <Container />} />}
-      />
-    </>
+    <Page
+     
+
+      body={() => (
+        
+      <Container width="100%" height="100%" child={() => <Center child={() => (
+        <>
+          
+        </>
+      )}/>
+      }/>
+      )}
+    />
   );
 }
