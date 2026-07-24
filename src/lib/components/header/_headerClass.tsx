60379445e6741873e576f9ec91459d15c2181ba3
ElktrumElk <festinacole373@gmail.com>
2026-07-24 21:43:39 +0000

    minor-minor: default font-size changed

--- src/lib/components/header/_headerClass.tsx
+++ src/lib/components/header/_headerClass.tsx
@@ -4,11 +4,13 @@ import type React from "react";
 export interface _HeaderProp {
   style?: React.CSSProperties;
   className?: string;
+  underLine?: string;
   leading?: React.JSX.ElementType;
   title: React.JSX.ElementType;
   subTitle?: React.JSX.ElementType;
   action?: React.JSX.ElementType;
   titleGap?: (string & `${string}rem`) | `${string}px` | `${string}em`;
+
 }
 
 export class _Header {
@@ -19,29 +21,34 @@ export class _Header {
   action!: React.JSX.ElementType;
 
   build? = ({ ...a }: _HeaderProp): React.JSX.Element => {
-    this.style = a.style || {
-      display: "flex",
-      justifyContent: "space-between",
-      width: "100%",
-      padding: ".5rem",
-      alignItems: 'center',
-      gap: '.5rem'
-    };
+    this.style = a.style || {};
 
     this.leading = a.leading as ElementType;
     this.action = a.action as ElementType;
     this.subTitle = a.subTitle as ElementType;
     this.title = a.title as ElementType;
-    
+
     return (
       <>
-        <header style={this.style} className={a.className}>
+        <header
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100%",
+            padding: ".5rem",
+            alignItems: "center",
+            gap: ".5rem",
+            borderBottom: a.underLine,
+            ...this.style,
+          }}
+          className={a.className}
+        >
           {a.leading && <this.leading />}
           <div
             style={{
               display: "flex",
               flexDirection: "column",
-              gap: (a.titleGap),
+              gap: a.titleGap,
               flex: "1",
             }}
           >
